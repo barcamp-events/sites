a8537aa70a57ec97fe7d2e251d88508c9f731253
Cara Michele <caraheacock@gmail.com>
2019-09-20 00:07:07 -0500

    Add fonts

--- 2019/.eleventy.js
+++ 2019/.eleventy.js
@@ -2,8 +2,12 @@ module.exports = function(eleventyConfig) {
   // Layouts
   eleventyConfig.addLayoutAlias('default', 'layouts/default.liquid');
 
+  // Passthrough copy
+  // Just copy this content into the _site directory
+  eleventyConfig.addPassthroughCopy("assets/fonts");
+
   return {
-    // passthroughFileCopy: true,
+    passthroughFileCopy: true,
     templateFormats : ["liquid"],
   }
 };
