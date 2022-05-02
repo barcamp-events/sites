22a5bad53f2f23d1dfb7c0f30b957b953ea9c303
William M. Riley <will@splitinfinities.com>
2022-05-01 19:18:42 -0500

    Updates to the template

--- _template/.eleventy.js
+++ _template/.eleventy.js
@@ -1,47 +1,86 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
-	// Custom Tags
-	eleventyConfig.addLiquidTag("agenda", function (liquidEngine) {
-		return { render: () => '<div class="flex flex-wrap flex-nowrap-ns">' };
-	});
+  eleventyConfig.addPassthroughCopy("manifest.json");
+  eleventyConfig.addPassthroughCopy("resume.pdf");
+  eleventyConfig.addPassthroughCopy("assets/**/*");
 
-	eleventyConfig.addLiquidTag("endagenda", function (liquidEngine) {
-		return { render: () => '</div>' };
-	});
+  // https://www.11ty.dev/docs/data-deep-merge/
+  eleventyConfig.setDataDeepMerge(true);
 
-	eleventyConfig.addLiquidTag("agendaday", function (liquidEngine) {
-		return {
-			parse: function (tagToken, remainingTokens) {
-				this.str = tagToken.args;
-			},
-			render: function (scope, hash) {
-				return (
-					'<div class="flex justify-center w-100 w-auto-ns mb4 mb0-ns">' +
-					`<div class="b--theme-base7 b--solid dm-b--theme-base9 black dm-white br-100 flex fs5 fw7 h3 items-center justify-center pa3 w3">${this.str}</div>` +
-					'</div>'
-				);
-			}
-		};
-	});
+  eleventyConfig.addFilter("isEnvironment", (env) => {
+    return process.env.VERCEL_ENV === env;
+  });
 
-	eleventyConfig.addLiquidTag("agendadetails", function (liquidEngine) {
-		return { render: () => '<div class="pt2 pl3-ns w-100">' };
-	});
-	eleventyConfig.addLiquidTag("endagendadetails", function (liquidEngine) {
-		return { render: () => '</div>' };
-	});
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL yyyy");
+  });
 
-	// Layouts
-	eleventyConfig.addLayoutAlias('en', 'en.liquid');
-	eleventyConfig.addLayoutAlias('es', 'es.liquid');
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
 
-	// Passthrough copy
-	// Just copy this content into the _site directory
-	eleventyConfig.addPassthroughCopy({ "node_modules/@stellar-design/core/dist/stellar-core": "assets/js/stellar-core" });
-	eleventyConfig.addPassthroughCopy("assets/**/*");
+  function elementFileNames(elements, esmBrowser) {
+    let files = [];
 
+    if (elements) {
+      elements.forEach((element) => {
+        const collectionBundles = esmBrowser.filter((esmB) => esmB.components.includes(element));
 
-	return {
-		passthroughFileCopy: true,
-		templateFormats: ["liquid"],
-	};
+        collectionBundles.forEach((bundle) => {
+          files = [...files, bundle.fileName, ...bundle.imports];
+        });
+      });
+    }
+
+    // Ensure all queued files are unique.
+    files = [...new Set(files)];
+
+    return files;
+  }
+  eleventyConfig.addFilter("elementFileNames", elementFileNames);
+
+  // Copy the `img` and `css` folders to the output\
+
+  // Passthrough copy
+  // Just copy this content into the _site directory
+  eleventyConfig.addPassthroughCopy({ "node_modules/@midwest-design/core/dist/core": "assets/js/midwest" });
+
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: ["md", "njk", "html", "liquid"],
+
+    // -----------------------------------------------------------------
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Don’t worry about leading and trailing slashes, we normalize these.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+
+    // Optional (default is shown)
+    pathPrefix: "/",
+    // -----------------------------------------------------------------
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
+    // Opt-out of pre-processing global data JSON files: (default: `liquid`)
+    dataTemplateEngine: false,
+
+    // These are all optional (defaults are shown):
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
 };
